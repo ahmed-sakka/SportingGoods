941059895b09a9a1556ee37f0fbbdde5378f4bd6
Ahmed Sakka <ahmedsakka720@gmail.com>
2021-01-16 16:42:22 +0100

    dashboard view

--- SportingGoods/Controllers/HomeController.cs
+++ SportingGoods/Controllers/HomeController.cs
@@ -6,20 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SportingGoods.Models;
+using SportingGoods.Models.Repositories;
 
 namespace SportingGoods.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        readonly IClientRepository _ClientRepository;
+        readonly IItemRepository _ItemRepository;
+        readonly IBillRepository _BillRepository;
+        readonly ICategoryRepository _CategoryRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClientRepository _ClientRepository, IItemRepository _ItemRepository, IBillRepository _BillRepository, ICategoryRepository _CategoryRepository)
         {
             _logger = logger;
+            this._ClientRepository = _ClientRepository;
+            this._ItemRepository = _ItemRepository;
+            this._BillRepository = _BillRepository;
+            this._CategoryRepository = _CategoryRepository;
         }
 
         public IActionResult Index()
         {
+            ViewBag.ClientsCount = _ClientRepository.GetAll().Count();
+            ViewBag.ItemsCount = _ItemRepository.GetAll().Count();
+            ViewBag.CategoriesCount = _CategoryRepository.GetAll().Count();
+            ViewBag.BillsCount = _BillRepository.GetAll().Count();
             return View();
         }
 
